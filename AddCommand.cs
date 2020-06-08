94182be19d99c0ffd7bdb89559f28b6ea66fc581
FinchyG <lagaunt120@yahoo.com>
2020-06-08 18:32:26 +0100

    first commit

---
+++ AddCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Bookshelf
+{
+    public class AddCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            var nameList = parameter as NameList;
+            var newName = string.Format("{0} {1}", nameList.FirstName, nameList.LastName);
+            nameList.Names.Add(newName);
+            nameList.FirstName = nameList.LastName = "";
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            var nameList = parameter as NameList;
+            return nameList != null
+                && !string.IsNullOrWhiteSpace(nameList.FirstName)
+                && !string.IsNullOrWhiteSpace(nameList.LastName);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ RemoveCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Bookshelf
+{
+    public class RemoveCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            var nameList = parameter as NameList;
+            return nameList != null && nameList.SelectedName != null;
+        }
+
+        public void Execute(object parameter)
+        {
+            var nameList = parameter as NameList;
+            var oldName = nameList.SelectedName;
+            nameList.Names.Remove(oldName);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
